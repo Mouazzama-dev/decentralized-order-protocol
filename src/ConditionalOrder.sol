9fc5f519cc4c3053556da3c251071d285e8bf0af
MouazzamaAnalog <mouazzama@analogmutations.com>
2023-10-29 00:43:57 +0500

    create and execute order logic

--- src/ConditionalOrder.sol
+++ src/ConditionalOrder.sol
@@ -2,8 +2,9 @@
 
 pragma solidity ^0.8.12;
 
-// Import OpenZeppelin's Pausable contract
+// Import OpenZeppelin's Pausable and ownable contract
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
 
 contract ConditionalOrder is Pausable {
 
@@ -33,7 +34,38 @@ contract ConditionalOrder is Pausable {
     event OrderExecuted(uint256 orderId);
 
     constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        owner = msg.sender;
+    }
+
+        function createOrder(
+        OrderType _orderType,
+        address _asset,
+        uint256 _amount,
+        Condition[] memory _conditions,
+        Logic _logic
+    ) public whenNotPaused returns (uint256) {
+        orderCount++;
+        orders[orderCount] = Order(msg.sender, _orderType, _asset, _amount, _conditions, _logic, false);
+        emit OrderCreated(orderCount, msg.sender);
+        return orderCount;
+    }
+
+    function executeOrder(uint256 _orderId) public whenNotPaused {
+        Order storage order = orders[_orderId];
+        require(!order.executed, "Order already executed");
+
+        uint256 conditionsMet = 0;
+        for (uint i = 0; i < order.conditions.length; i++) {
+            if (checkCondition(order.conditions[i])) {
+                conditionsMet++;
+            }
+        }
+
+        if (order.logic == Logic.AND && conditionsMet == order.conditions.length) {
+            performTrade(order);
+        } else if (order.logic == Logic.OR && conditionsMet > 0) {
+            performTrade(order);
+        }
     }
 
 }
