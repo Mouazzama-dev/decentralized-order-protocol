8d7aa854de37f588f1bde8019754fe4dc9df429e
MouazzamaAnalog <mouazzama@analogmutations.com>
2023-10-29 13:15:55 +0500

    improved code

--- src/ConditionalOrder.sol
+++ src/ConditionalOrder.sol
@@ -2,61 +2,66 @@
 
 pragma solidity ^0.8.12;
 
-// Import OpenZeppelin's Pausable and ownable contract
+// Import required OpenZeppelin and Chainlink contracts
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
+// A contract to execute conditional orders based on various conditions
 contract ConditionalOrder is Pausable, Ownable {
 
+    // Interface for Chainlink price feeds
     AggregatorV3Interface internal priceFeed;
 
-    enum OrderType {
-        Buy,
-        Sell
-    }
-    enum ConditionType {
-        TimeBased,
-        EventBased,
-        PriceBased
-    }
-    enum Logic {
-        AND,
-        OR
-    }
+     // Define order types: Buy or Sell
+    enum OrderType {Buy,Sell}
 
+    // Define condition types: Time-Based, Event-Based, or Price-Based
+    enum ConditionType {TimeBased, EventBased, PriceBased}
+
+    // Logic for checking conditions: AND or OR
+    enum Logic {AND, OR}
+
+    // Structure to define a condition
     struct Condition {
-        ConditionType conditionType;
-        uint256 value;
+        ConditionType conditionType;    // Type of the condition
+        uint256 value;                  // Value for the condition (e.g., timestamp, price)
     }
 
     struct Order {
-        address user;
-        OrderType orderType;
-        address asset;
-        uint256 amount;
-        Condition[] conditions;
-        Logic logic;
-        bool executed;
+        address user;               // Address of the user who created the order
+        OrderType orderType;        // Type of the order (Buy/Sell)
+        address asset;              // Address of the asset involved
+        uint256 amount;             // Amount of the asset involved
+        Condition[] conditions;     // Array of conditions for the order
+        Logic logic;                // Logic to check conditions
+        bool executed;              // Status of the order (executed or not)
     }
 
     uint256 public orderCount = 0;
-    mapping(uint256 => Order) public orders;
+    mapping(uint256 => Order) public orders; // Mapping from order ID to Order structure
 
+    // Events for order creation, execution, and trade
     event OrderCreated(uint256 orderId, address indexed user);
     event OrderExecuted(uint256 orderId);
     event TradeExecuted(uint256 orderId, OrderType orderType, address asset, uint256 amount);
-
-
-    // constructor() {
-    //     owner = msg.sender;
-    // }
+    
+    // Constructor to initialize the price feed
     constructor() Ownable(msg.sender) {
-            priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
 
     }
+
+    /**
+    * @dev Creates a new conditional order
+    * @param _orderType Type of the order (Buy or Sell)
+    * @param _asset Address of the asset involved in the order
+    * @param _amount Amount of the asset involved
+    * @param _conditions Array of conditions for the order
+    * @param _logic Logic to evaluate conditions (AND/OR)
+    *
+    * @return Order ID of the created order
+    */
 
     function createOrder(
         OrderType _orderType,
@@ -64,52 +69,67 @@ contract ConditionalOrder is Pausable, Ownable {
         uint256 _amount,
         Condition[] memory _conditions,
         Logic _logic
-    ) public whenNotPaused returns (uint256) {
-        orderCount++;
-        Order storage newOrder = orders[orderCount];
-        newOrder.user = msg.sender;
-        newOrder.orderType = _orderType;
-        newOrder.asset = _asset;
-        newOrder.amount = _amount;
-        newOrder.logic = _logic;
-        newOrder.executed = false;
-        
-        // Manually copy each condition from memory to storage.
-        for (uint i = 0; i < _conditions.length; i++) {
-            newOrder.conditions.push(_conditions[i]);
-        }
+        ) public whenNotPaused returns (uint256) {
+            orderCount++;
+            Order storage newOrder = orders[orderCount];
+            newOrder.user = msg.sender;
+            newOrder.orderType = _orderType;
+            newOrder.asset = _asset;
+            newOrder.amount = _amount;
+            newOrder.logic = _logic;
+            newOrder.executed = false;
 
-        emit OrderCreated(orderCount, msg.sender);
-        return orderCount;
+            for (uint i = 0; i < _conditions.length; i++) {
+                newOrder.conditions.push(_conditions[i]);
+            }
+
+            emit OrderCreated(orderCount, msg.sender);
+            return orderCount;
     }
 
+    /**
+    * @dev Fetches details of a specific order
+    * @param _orderId ID of the order to fetch
+    *
+    * @return Details of the specified order
+    */
+
     function getOrder(uint256 _orderId) external view returns (
-    address user,
-    OrderType orderType,
-    address asset,
-    uint256 amount,
-    Condition[] memory conditions,
-    Logic logic,
-    bool executed
-) {
-    Order storage order = orders[_orderId];
-    return (
-        order.user,
-        order.orderType,
-        order.asset,
-        order.amount,
-        order.conditions,
-        order.logic,
-        order.executed
-    );
-}
+        address user,
+        OrderType orderType,
+        address asset,
+        uint256 amount,
+        Condition[] memory conditions,
+        Logic logic,
+        bool executed
+        ) {
+            Order storage order = orders[_orderId];
+            return (
+                order.user,
+                order.orderType,
+                order.asset,
+                order.amount,
+                order.conditions,
+                order.logic,
+                order.executed
+            );
+    }
 
-function getLatestPrice() internal view returns (int) {
-    (,int price,,,) = priceFeed.latestRoundData();
-    return price;
-}
+    /**
+    * @dev Fetches the latest price from Chainlink price feed
+    * 
+    * @return Latest price
+    */
+    function getLatestPrice() internal view returns (int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return price;
+    }
 
 
+        /**
+    * @dev Executes a conditional order if its conditions are met
+    * @param _orderId ID of the order to execute
+    */
 
     function executeOrder(uint256 _orderId) public whenNotPaused {
         Order storage order = orders[_orderId];
@@ -134,48 +154,49 @@ function getLatestPrice() internal view returns (int) {
         }
     }
 
-    // function checkCondition(
-    //     Condition memory _condition
-    // ) internal view returns (bool) {
-    //     if (_condition.conditionType == ConditionType.TimeBased) {
-    //         return block.timestamp >= _condition.value;
-    //     }
-    //     return false;
-    //     // ... get data from some other resource like oracle
-    // }
+    /**
+    * @dev Checks if a specific condition of an order is met
+    * @param _condition Condition to check
+    *
+    * @return True if condition is met, otherwise false
+    */
 
     function checkCondition(Condition memory _condition) internal view returns (bool) {
-    if (_condition.conditionType == ConditionType.TimeBased) {
-        return block.timestamp >= _condition.value;
-    }
+        if (_condition.conditionType == ConditionType.TimeBased) {
+            return block.timestamp >= _condition.value;
+        }
     
-    if (_condition.conditionType == ConditionType.PriceBased) {
-        int latestPrice = getLatestPrice();
-        // Assuming value is the threshold price for the condition
-        return latestPrice >= int(_condition.value); // Needs careful consideration if using other than ETH/USD
+        if (_condition.conditionType == ConditionType.PriceBased) {
+            int latestPrice = getLatestPrice();
+            // Assuming value is the threshold price for the condition
+            return latestPrice >= int(_condition.value); // Needs careful consideration if using other than ETH/USD
+        }
+
+        // EventBased can be tricky as it depends on what event you are looking for.
+        // You might need another oracle or a method to set the event status on-chain.
+        if (_condition.conditionType == ConditionType.EventBased) {
+            // Placeholder, implement based on the specific event.
+            // return some_event_happened == _condition.value;
+        }
+
+        return false;
     }
 
-    // EventBased can be tricky as it depends on what event you are looking for.
-    // You might need another oracle or a method to set the event status on-chain.
-    if (_condition.conditionType == ConditionType.EventBased) {
-        // Placeholder, implement based on the specific event.
-        // return some_event_happened == _condition.value;
-    }
-
-    return false;
-}
-
+    /**
+    * @dev Performs a trade for a given order (placeholder logic)
+    * @param order Order to perform the trade for
+    */
 
    function performTrade(Order storage order) internal {
-    // Placeholder logic for trade execution.
-    // In a real-world scenario, this would integrate with an exchange or a DeFi protocol.
+        // Placeholder logic for trade execution.
+        // In a real-world scenario, this would integrate with an exchange or a DeFi protocol.
     
-    if (order.orderType == OrderType.Buy) {
+        if (order.orderType == OrderType.Buy) {
         // Simulate buying the asset for the user
-        emit TradeExecuted(orderCount, OrderType.Buy, order.asset, order.amount);
-    } else if (order.orderType == OrderType.Sell) {
-        // Simulate selling the asset for the user
-        emit TradeExecuted(orderCount, OrderType.Sell, order.asset, order.amount);
+            emit TradeExecuted(orderCount, OrderType.Buy, order.asset, order.amount);
+        } else if (order.orderType == OrderType.Sell) {
+            // Simulate selling the asset for the user
+            emit TradeExecuted(orderCount, OrderType.Sell, order.asset, order.amount);
+        }
     }
-   }
 }
