23a40f1ef2330fa2de087dc2a1874b15c90abe8d
MouazzamaAnalog <mouazzama@analogmutations.com>
2023-10-29 13:44:42 +0500

    create price based order

--- test/conditionalorder.t.sol
+++ test/conditionalorder.t.sol
@@ -97,6 +97,54 @@ function test_executeOrderTrade() public {
     // For this example, the emitted event would be the evidence of "trade execution".
 }
 
+function test_createOrderPriceBased() public {
+    // Initializing conditions array with a specific size
+    ConditionalOrder.Condition[] memory initialConditions = new ConditionalOrder.Condition[](1);
+    
+    // Here we assume that if the asset price goes above 2000 units (e.g., if you're using ETH/USD feed) the order should execute.
+    ConditionalOrder.Condition memory condition = ConditionalOrder.Condition({
+        conditionType: ConditionalOrder.ConditionType.PriceBased,
+        value: 2000 
+    });
+    
+    initialConditions[0] = condition;
+
+    uint256 orderId = conditionalOrder.createOrder(
+        ConditionalOrder.OrderType.Buy,
+        address(0),
+        100,
+        initialConditions,
+        ConditionalOrder.Logic.AND
+    );
+
+    // Asserting order ID
+    assertEq(orderId, 1);
+
+    // Fetching the order
+    (address user, ConditionalOrder.OrderType orderType, address asset, uint256 amount, ConditionalOrder.Condition[] memory fetchedConditions, ConditionalOrder.Logic logic, bool executed) = conditionalOrder.getOrder(orderId);
+
+    ConditionalOrder.Order memory order = ConditionalOrder.Order({
+        user: user,
+        orderType: orderType,
+        asset: asset,
+        amount: amount,
+        conditions: fetchedConditions,
+        logic: logic,
+        executed: executed
+    });
+
+    // Asserting order details
+    assertEq(order.user, address(this));
+    assertEq(uint(order.orderType), uint(ConditionalOrder.OrderType.Buy));
+    assertEq(order.asset, address(0));
+    assertEq(order.amount, 100);
+    assertEq(order.conditions.length, 1);
+    assertEq(uint(order.conditions[0].conditionType), uint(ConditionalOrder.ConditionType.PriceBased));
+    assertEq(order.conditions[0].value, condition.value);
+    assertEq(uint(order.logic), uint(ConditionalOrder.Logic.AND));
+    assertTrue(!order.executed);
+}
+
 
 
 }
